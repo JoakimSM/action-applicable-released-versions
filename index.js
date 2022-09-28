6558b5300155306020d179e872c3f49befb12bd5
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2022-09-28 14:33:43 +0200

    chore: up

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const core = require('@actions/core')
 try {
     const devVersion = Number(core.getInput('dev-version'))
     const { minDHIS2Version } = require('./d2.config.js')
-    const minMinorVersion = Number(/[.](\d+)/.exec(minDHIS2Version)[1])
+
+    const majorVersion = /^\d+/.exec(devVersion)[0]
+
+    const minorVersionRegex = /[.](\d+)/
+    const devMinorVersion = Number(minorVersionRegex.exec(devVersion)[1])
+    const minMinorVersion = Number(minorVersionRegex.exec(minDHIS2Version)[1])
 
     const versions = []
-    for (let index = minMinorVersion; index < devVersion; index++) {
-        versions.push(index)
+    for (let index = minMinorVersion; index < devMinorVersion; index++) {
+        versions.push(`${majorVersion}.${index}`)
     }
-    core.info('output-versions: ' + JSON.stringify(versions))
+    core.info(`output-versions: ${JSON.stringify(versions)}`)
     core.setOutput('versions', versions)
 } catch (error) {
     core.setFailed(error.message)
