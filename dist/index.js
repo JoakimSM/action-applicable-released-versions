7d8a15d5dcec17aad58bff3ea9a3681a2aee0295
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2022-09-28 14:50:12 +0200

    chore: up

--- dist/index.js
+++ dist/index.js
@@ -2825,18 +2825,14 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(186)
 
 try {
-    const devVersion = Number(core.getInput('dev-version'))
+    const devVersion = core.getInput('dev-version')
     const { minDHIS2Version } = __nccwpck_require__(379)
-    core.info(devVersion + ' ' + minDHIS2Version)
-    /*
+
     const majorVersion = /^\d+/.exec(devVersion)[0]
-    core.info('0:' + majorVersion)
 
     const minorVersionRegex = /[.](\d+)/
     const minMinorVersion = Number(minorVersionRegex.exec(minDHIS2Version)[1])
     const devMinorVersion = Number(minorVersionRegex.exec(devVersion)[1])
-
-    core.info('1: ' + minMinorVersion + ' 2:' + devMinorVersion)
 
     const versions = []
     for (let index = minMinorVersion; index < devMinorVersion; index++) {
@@ -2844,7 +2840,6 @@ try {
     }
     core.info(`output-versions: ${JSON.stringify(versions)}`)
     core.setOutput('versions', versions)
-    */
 } catch (error) {
     core.setFailed(error.message)
 }
--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const core = require('@actions/core')
 
 try {
-    const devVersion = Number(core.getInput('dev-version'))
+    const devVersion = core.getInput('dev-version')
     const { minDHIS2Version } = require('./d2.config.js')
-    core.info(devVersion + ' ' + minDHIS2Version)
-    /*
+
     const majorVersion = /^\d+/.exec(devVersion)[0]
-    core.info('0:' + majorVersion)
 
     const minorVersionRegex = /[.](\d+)/
     const minMinorVersion = Number(minorVersionRegex.exec(minDHIS2Version)[1])
     const devMinorVersion = Number(minorVersionRegex.exec(devVersion)[1])
-
-    core.info('1: ' + minMinorVersion + ' 2:' + devMinorVersion)
 
     const versions = []
     for (let index = minMinorVersion; index < devMinorVersion; index++) {
@@ -20,7 +16,6 @@ try {
     }
     core.info(`output-versions: ${JSON.stringify(versions)}`)
     core.setOutput('versions', versions)
-    */
 } catch (error) {
     core.setFailed(error.message)
 }
