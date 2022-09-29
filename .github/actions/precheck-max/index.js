982c772a40b2f4541c16f18a6e5f5db9636242a2
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2022-09-29 18:18:06 +0200

    chore: up

--- .github/actions/precheck-max/index.js
+++ .github/actions/precheck-max/index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core')
 
 try {
+    const a = process.env.GITHUB_WORKSPACE;
+    core.info('a', a);
     const { maxDHIS2Version } = require('./d2.config.js')
     core.setOutput('max-version', maxDHIS2Version || '')
 } catch (error) {
